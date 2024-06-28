067c57b4d3bfac06932f8256ba6cc3aeb02dbba2
ssb <sb1664@gmail.com>
2024-06-28 15:35:09 +0100

    Initial commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,6 @@ import { cookies } from 'next/headers';
 import { LIGHT_COLORS, DARK_COLORS } from '@/constants';
 
 import DarkLightToggle from '@/components/DarkLightToggle';
-import ThemeProvider from '@/components/ThemeProvider';
 
 import './styles.css';
 
@@ -18,13 +17,11 @@ function RootLayout({ children }) {
   return (
     <html lang='en' data-color-theme={theme} style={themeColors}>
       <body>
-        <ThemeProvider initialTheme={theme}>
-          <header className='site-header'>
-            <Link href=''>Some Website</Link>
-            <DarkLightToggle />
-          </header>
-          {children}
-        </ThemeProvider>
+        <header className='site-header'>
+          <Link href=''>Some Website</Link>
+          <DarkLightToggle initialTheme={theme} />
+        </header>
+        {children}
       </body>
     </html>
   );
